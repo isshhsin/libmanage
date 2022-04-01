c79fc517b6bfdf142eb0b838482fc96246604b0b
isshhsin <102480878+isshhsin@users.noreply.github.com>
2022-04-01 19:53:22 +0530

    Update AddBookController.java

--- src/main/java/com/nagarro/controllers/AddBookController.java
+++ src/main/java/com/nagarro/controllers/AddBookController.java
@@ -46,8 +46,10 @@ public class AddBookController {
 		String name = request.getParameter("bookname");
 		String author = request.getParameter("author");
 		
+		
 		String data = "name="+name+"&author="+author;
 		System.out.println();
+		System.out.println(" ");
 		Book book = new Book(name,author);
 		ResponseEntity<Book> b = restTemplate.postForEntity("http://localhost:8090/addBook", book, Book.class);
 		System.out.println(b.getBody());
